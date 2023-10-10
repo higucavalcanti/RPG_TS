faf7c993e25fede0255f0b6098646e78788ed0c0
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-10 18:40:44 -0300

    visualizando espaço do Character no mapa

--- rpg/src/App.tsx
+++ rpg/src/App.tsx
@@ -1,10 +1,11 @@
 import * as C from './App.styles'
+import { Character } from './components/Character';
 
 const App = () => {
   return (
     <C.Container>
       <C.Map>
-        
+        <Character />
       </C.Map>
     </C.Container>
   );
